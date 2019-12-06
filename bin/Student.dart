a40e3f04ddee9fff7fd6fe9292539b4e00cff4b0
fatma mohsen <fatmaamohsen.999@gmail.com>
2019-12-06 17:42:15 +0200

    modifying

--- bin/Student.dart
+++ bin/Student.dart
@@ -5,9 +5,12 @@
     String level;
     int grade;
     String gender;
+
     Student(this.id, this.name, this.level);
+
     Student.all(this.id, this.name, this.age, this.level, this.grade,
         this.gender);
+
     String studentGrade(int subject1, int subject2, int subject3, int subject4) {
       int calculate = subject1 + subject2 + subject3 + subject4;
       return (calculate.toString());
@@ -18,15 +21,20 @@
        bool attendStudent(List l){
       int countYes = 0;
       int countNo = 0;
+
       for(int i = 0; i<l.length; i++) {
         if (l[i] == "yes") {
           countYes++;
         } else {
           countNo++;
-        }}
+        }
+      }
         if(countYes>=countNo){
           return true;
         }else{
-          return false; }}}
+          return false;
+        }
+    }
+}
 
 
